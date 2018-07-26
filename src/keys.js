b035f0e37c0a49a2cf4730fa75e7bdd06cc488d8
bartosz <abraxaswisz@gmail.com>
2018-07-26 18:29:48 +0200

    recreating whole styling thing

---
+++ src/keys.js
@@ -0,0 +1,7 @@
+const config = {
+  apiKey: "AIzaSyA67AlYhLvxXoriIRvj_UQP089TKJV8DnU",
+  authDomain: "to-do-list-ea720.firebaseapp.com",
+  databaseURL: "https://to-do-list-ea720.firebaseio.com"
+};
+
+export default config;

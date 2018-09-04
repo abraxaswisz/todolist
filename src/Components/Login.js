61991dc2e868381255c421a84e165f7261f1e42a
abraxaswisz <abraxaswisz@gmail.com>
2018-09-04 20:01:38 +0200

    made redirect async and removed Redirect

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -58,8 +58,8 @@ class Login extends Component {
     });
   }
 
-  redirectToDashboard = () => {
-    return <Redirect to={Dashboard} />;
+  redirectToDashboard = async () => {
+    return <Dashboard />;
   };
 
   loginMethod = e => {
